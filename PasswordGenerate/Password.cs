df5f1f78bf935dd94c891c8b65c9ff6c7f040a5a
Moises Moreira <92399378+moises-moreira@users.noreply.github.com>
2023-10-29 15:14:20 -0300

    files add

---
+++ PasswordGenerate/Password.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerate
+{
+    internal class Password
+    {
+        private string caractersAllow = "ABCDEFGHIJKLMNOPQRSTUVXIZÇ" +
+            "ABCDEFGHIJKLMNOPQRSTUVXIZÇ".ToLower() +
+            "0123456789" + @"/*-+!@#$%¨&*()_\[]{}";
+        private string newPasswordGenerated = "";
+        private int index;
+
+        public string GeneratePassword()
+        {
+            newPasswordGenerated = "";
+            Random random = new Random();
+            int caractersLenght = 8;
+            for (int i = 0; i < caractersLenght; i++)
+            {
+                index = random.Next(0, caractersAllow.Length -1);
+                newPasswordGenerated += caractersAllow[index];
+            }
+            return newPasswordGenerated;
+        }
+    }
+}
---
+++ PasswordGenerate/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace PasswordGenerate
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Password classPass = new Password();
+
+            while (true)
+            {
+                Console.WriteLine("Gerador de senhas");
+                Console.Write("para gerar (S/s)im ou (N/n)ao: ");
+                string? input = Console.ReadLine();
+
+                if (input != "")
+                {
+                    if (input == "Sim" || input == "sim" || input == "S" || input == "s")
+                    {
+                        Console.WriteLine("Senha gerada!!!!!");
+                        string passTemp = classPass.GeneratePassword();
+                        
+                        Console.WriteLine(passTemp);
+                        Console.WriteLine("Deseja salvar a senha gerada: ");
+                        string? outInput = Console.ReadLine();
+
+                        if(outInput != "")
+                        {
+                            if (outInput == "Sim" || outInput == "sim" || outInput == "S" || outInput == "s")
+                            {
+                                Console.WriteLine("Senha salva");
+
+                                if (!File.Exists(@"./pass.txt"))
+                                {
+                                    File.Create(@"./pass.txt");
+                                }
+
+                                string[] pass = File.ReadAllLines(@"./pass.txt");
+
+                                List<string> output = new List<string>();
+
+                                foreach (string item in pass)
+                                {
+                                    output.Add(item);
+                                }
+
+                                output.Add(passTemp);
+
+                                File.WriteAllLinesAsync (@"./pass.txt", output).Wait();
+
+                                Console.WriteLine(@"Arquivo salvo: " + AppDomain.CurrentDomain.BaseDirectory + @"pass.txt");
+                                Console.ReadKey();
+                                Console.Clear();
+                                continue;
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Senha não salva!");
+                                continue;
+                            }
+                        }
+                    }
+                    if(input == "Nao" || input == "nao" || input == "N" || input == "n")
+                    {
+                        Console.WriteLine("Encerrando App");
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Respoda apenas com: (S/s)im ou (N/n)ao");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Respoda apenas com: (S/s)im ou (N/n)ao");
+                }
+            }
+        }
+    }
+}
